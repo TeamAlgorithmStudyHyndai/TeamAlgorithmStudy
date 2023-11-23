e3142951bb2d8bc31b55bea6f3b6f16e8bbe7100
wonsun <98735772+ws1811@users.noreply.github.com>
2023-11-23 11:09:13 +0900

    Create 헤비 유저가 소유한 장소.sql

---
+++ TeamAlgorithmStudy/Week 3/Wonsun/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+SELECT  id
+        ,name
+        ,host_id
+FROM    places
+WHERE   host_id in(SELECT      host_id
+                 FROM        places
+                 GROUP BY    host_id
+                 HAVING      count(*) >= 2)

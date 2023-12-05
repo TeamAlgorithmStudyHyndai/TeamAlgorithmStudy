1cbc4cf65450459bce15bd0dd47d3ada40f835f5
Eunseo Kim <alan0221@naver.com>
2023-12-05 22:28:55 +0900

    Update Eunseo.sql

--- TeamAlgorithmStudy/Week 5/SQL/저자 별 카테고리 별 매출액 집계하기/Eunseo.sql
+++ TeamAlgorithmStudy/Week 5/SQL/저자 별 카테고리 별 매출액 집계하기/Eunseo.sql
@@ -1,1 +1,5 @@
---
+SELECT  a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(d.SALES * b.PRICE) as TOTAL_SALES 
+from (author a join book b on a.AUTHOR_ID = b.AUTHOR_ID) join BOOK_SALES  d on b.BOOK_ID = d.book_id
+where to_char(d.SALES_DATE, 'yyyymm') = '202201'
+group by a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
+order by a.AUTHOR_ID, b.CATEGORY desc;
